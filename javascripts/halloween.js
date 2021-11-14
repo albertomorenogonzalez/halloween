6e7ae6a59aaf23dd3b6b149a41d237d265daad86
Alberto Moreno González <91873488+albertomorenogonzalez@users.noreply.github.com>
2021-11-14 13:10:44 +0100

    Añadidos archivos de la página de halloween

---
+++ javascripts/halloween.js
@@ -0,0 +1,99 @@
+/********* Aquí están las funciones de Halloween *********/ 
+
+/********* una función es un módulo que ejecuta una sola tarea
+ *         mostrar --> muestra, ocultar --> oculta
+ *         Javascrip toma como objeto principal document, que 
+ *         es la página html actual
+ *         tiene una serie de operadores, llamados getters,
+ *         que seleccionan elementos para su manipulación.
+ *         Un elemento, seleccionado como getter, puede ser 
+ *         modificado: contenido, estilo, los atributos, ...
+ */
+
+function sonar (musicaFondo1) {
+    document.getElementById(musicaFondo1).play();
+}
+
+function mostrar (screamer) {
+    document.getElementById(screamer).style.visibility="visible"
+}
+
+function ocultar (screamer) {
+    document.getElementById(screamer).style.visibility="hidden"
+}
+
+function sonar (screamerad) {
+    document.getElementById(screamerad).play();
+}
+
+function parar (musicaFondo1) {
+    document.getElementById(musicaFondo1).pause();
+}
+
+function sonar (and_yell) {
+    document.getElementById(and_yell).play();
+}
+
+function mostrar (spScSk1) {
+    document.getElementById(spScSk1).style.visibility="visible"
+}
+
+function ocultar (spScSk1) {
+    document.getElementById(spScSk1).style.visibility="hidden"
+}
+
+function mostrar (spScSk2) {
+    document.getElementById(spScSk2).style.visibility="visible"
+}
+
+function ocultar (spScSk2) {
+    document.getElementById(spScSk2).style.visibility="hidden"
+}
+
+function mostrar (spScSk3) {
+    document.getElementById(spScSk3).style.visibility="visible"
+}
+
+function ocultar (spScSk3) {
+    document.getElementById(spScSk3).style.visibility="hidden"
+}
+
+function sonar(spScSk) {
+    document.getElementById(spScSk).play();
+}
+
+function parar (spScSk) {
+    document.getElementById(spScSk).pause();
+}
+
+function mostrar (p4) {
+    document.getElementById(p4).style.visibility="visible"
+}
+
+function ocultar (p4) {
+    document.getElementById(p4).style.visibility="hidden"
+}
+
+function mostrar (p4v2) {
+    document.getElementById(p4v2).style.visibility="visible"
+}
+
+function ocultar (p4v2) {
+    document.getElementById(p4v2).style.visibility="hidden"
+}
+
+function mostrar (p5) {
+    document.getElementById(p5v2).style.visibility="visible"
+}
+
+function ocultar (p5) {
+    document.getElementById(p5v2).style.visibility="hidden"
+}
+
+function mostrar (p5v2) {
+    document.getElementById(p5v2).style.visibility="visible"
+}
+
+function ocultar (p5v2) {
+    document.getElementById(p5v2).style.visibility="hidden"
+}
---
+++ javascripts/halloween2.js
@@ -0,0 +1,37 @@
+/********* Aquí están las funciones de Halloween *********/
+
+function sonar (musicaFondo2) {
+    document.getElementById(musicaFondo2).play();
+}
+
+function ocultar (ghost) {
+    document.getElementById(ghost).style.visibility="hidden"
+}
+
+function mostrar (p1) {
+    document.getElementById(p1).style.visibility="visible"
+}
+
+function ocultar (p1) {
+    document.getElementById(p1).style.visibility="hidden"
+}
+
+function parar (musicaFondo2) {
+    document.getElementById(musicaFondo2).pause();
+}
+
+function sonar (dsgt) {
+    document.getElementById(dsgt).play();
+}
+
+function mostrar (you_died) {
+    document.getElementById(you_died).style.visibility="visible"
+}
+
+function sonar (died) {
+    document.getElementById(died).play();
+}
+
+function ocultar (gwyndolin) {
+    document.getElementById(gwyndolin).style.visibility="hidden"
+}
